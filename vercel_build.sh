ed3cefdd2456f34f4d638746ff3826a7dd735f44
saifullahawan865 <230993@students.au.edu.pk>
2025-10-18 02:24:33 +0500

    Initial commit with Flutter project and Vercel setup

---
+++ vercel_build.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+# Install Flutter SDK
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Get dependencies
+flutter pub get
+
+# Build for web
+flutter build web --release
